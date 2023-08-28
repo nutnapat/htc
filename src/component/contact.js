fb6a1553997996a833cdc0834d32b2156ae88f4d
nut <napat@konsys.co>
2023-08-28 10:53:23 +0700

    change ig img

--- src/component/contact.js
+++ src/component/contact.js
@@ -1,9 +1,7 @@
-import igImage from '../image/ig.PNG';
-
 const contactComponent = () => {
     return (
         <div class="image-line">
-            <a href="https://instagram.com/htc.chula?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noreferrer"> <img src={igImage} alt='IG' /> </a>
+            <a href="https://instagram.com/htc.chula?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noreferrer"> <img src={require('../image/ig.PNG')} alt='IG' /> </a>
             <a href="https://www.facebook.com/HilltribeclubCU?mibextid=LQQJ4d" target="_blank" rel="noreferrer"> <img src={require('../image/fb.PNG')} alt='FB' /> </a>
             <a href="https://twitter.com/htcchula" target="_blank" rel="noreferrer"> <img src={require('../image/tw.PNG')} alt='TW' /> </a>
             <a href="http://www.tiktok.com/@htc.chula" target="_blank" rel="noreferrer"> <img src={require('../image/tt.PNG')} alt='TT' /> </a>
